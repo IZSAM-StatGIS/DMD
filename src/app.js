9fa0eb6403c837d20af4fb396b992f9e031e2e86
Alessio Di Lorenzo <alessio.dl@gmail.com>
2020-07-22 10:37:49 +0200

    Application main modules in place

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-console.log('testing app.js file')
+import './scss/main.scss';
+import '@fortawesome/fontawesome-free/js/all'
+// Bootstrap
+import $ from 'jquery'
+import 'popper.js'
+import 'bootstrap'
+// Bootstrap select
+import 'bootstrap-select';
+$.fn.selectpicker.Constructor.BootstrapVersion = '4';
+import 'bootstrap-select/dist/css/bootstrap-select.min.css';
+
+import './js/map';
